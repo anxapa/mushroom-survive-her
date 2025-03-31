71f70e2285ad5ca5fbd48a074248aef783cd28d4
a-robin23 <148491884+a-robin23@users.noreply.github.com>
2025-03-30 17:21:33 -0700

    Revamped enemy manager
    - Added enemy manager node for debug purposes
    - Added various functions for enemy spawn management

--- Scripts/Enemy/enemy_manager.gd
+++ Scripts/Enemy/enemy_manager.gd
@@ -1,26 +1,28 @@
 extends Node2D
 var player = GameManager.get_player()
 #var rng = RandomNumberGenerator.new()
-var base_frequency := 5.0
+var base_frequency := 11.0
 # time elapsed in seconds
 var time_elapsed := 0.0
 var can_spawn := true
+var difficulty = 1
 @export var cluster_size := 1
 const ant = preload("res://scenes/Enemy/ant_mob.tscn")
 const charger = preload("res://Scenes/Enemy/charger_mob.tscn")
 const shooter = preload("res://Scenes/Enemy/shooter_mob.tscn")
-var enemies = [shooter]
+const minute := 60.0
+var enemies := []
 
 func _ready() -> void:
+	mob_change(1, 0, 0)
 	spawn_mob()
 	
 func _physics_process(delta: float) -> void:
 	if can_spawn:
 		spawn_mob()
 	time_elapsed += delta
-	if time_elapsed > 5 and not enemies.has(charger):
-		enemies.push_back(charger)
-	pass
+	if time_elapsed/60 == difficulty:
+		difficulty_change()
 		
 func spawn_mob() -> void:
 	can_spawn = false
@@ -28,5 +30,56 @@ func spawn_mob() -> void:
 		var enemy = enemies.pick_random()
 		enemy = enemy.instantiate()
 		add_child(enemy)
-	await get_tree().create_timer(base_frequency).timeout
+		if difficulty >= 4:
+			make_variant(enemy)
+	await get_tree().create_timer(base_frequency - difficulty).timeout
 	can_spawn = true
+	
+func make_variant(enemy: Enemy) -> void:
+	if randf_range(difficulty, 20)/2 > 0.5:
+		match randi_range(1, 3):
+			1:
+				enemy.make_brute()
+			2:
+				enemy.make_rogue()
+			3:
+				enemy.make_tank()
+				
+## arguments determine spawn ratios.
+## i.e. mob_change(1, 1, 1) is a uniform distribution.
+func mob_change(ant: int, charger: int, shooter: int) -> void:
+	enemies.clear()
+	for n in ant:
+		enemies.push_back(self.ant)
+	for n in charger:
+		enemies.push_back(self.charger)
+	for n in shooter:
+		enemies.push_back(self.shooter)
+	
+func difficulty_change() -> void:
+	difficulty += 1
+	match difficulty:
+		2:
+			mob_change(2, 1, 0)
+		3:
+			mob_change(4, 5, 0)
+		4:
+			await get_tree().create_timer(10).timeout
+			mob_change(1, 0, 0)
+		5:
+			mob_change(1, 0, 0)
+			await get_tree().create_timer(10).timeout
+			mob_change(1, 1, 0)
+		6:
+			mob_change(1, 0, 1)
+			await get_tree().create_timer(10).timeout
+			mob_change(1, 1, 1)
+		7:
+			mob_change(1, 1, 2)
+		8:
+			mob_change(1, 2, 2)
+		9:
+			mob_change(1, 2, 2)
+		10:
+			mob_change(1, 1, 1)
+	
